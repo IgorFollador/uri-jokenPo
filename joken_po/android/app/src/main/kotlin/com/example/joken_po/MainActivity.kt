17b2a205d636b55f7d0a6963dfc4c6ef45370bd5
Igor Follador <igorledf@hotmail.com>
2022-08-24 19:00:29 -0300

    Move files

--- joken_po/android/app/src/main/kotlin/com/example/joken_po/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.joken_po
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- joken_po/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:joken_po/game.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: Game(),
-    debugShowCheckedModeBanner: false,
-  ));
-}
